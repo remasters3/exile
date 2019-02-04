d2e2700c704ac6d11fb25108078865aa3451da08
gerryfranchetti@gmail.com <gerryfranchetti@gmail.com>
2019-02-04 19:00:35 +0000

    04/02/2019 - 19:00:35.14

--- exile.malden/config.cpp
+++ exile.malden/config.cpp
@@ -3725,7 +3725,7 @@ class CfgExileMobileXM8
 	 * Add the 4 digit permission code here, so players can authorize
 	 * your server to send them messages to their Mobile XM8 app.
 	 */
-	code = "4444";
+	code = "BTIR";
 };
 class CfgExileMusic
 {
